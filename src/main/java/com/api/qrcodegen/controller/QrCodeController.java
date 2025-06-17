edcfdf9457e8124522534c84533aa6497102200e
otaviocostao <dev.otaviocostao@gmail.com>
2025-06-17 18:42:26 -0300

    Configuração do docker + configuração das credenciais

--- src/main/java/com/api/qrcodegen/controller/QrCodeController.java
+++ src/main/java/com/api/qrcodegen/controller/QrCodeController.java
@@ -2,18 +2,35 @@ package com.api.qrcodegen.controller;
 
 import com.api.qrcodegen.dto.qrcode.QrCodeGenerateRequest;
 import com.api.qrcodegen.dto.qrcode.QrCodeGenerateResponse;
+import com.api.qrcodegen.service.QrCodeGeneratorService;
+import com.google.zxing.WriterException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
+
     @PostMapping
-    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request){
-        return null;
+    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) throws IOException, WriterException {
+        try{
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            System.out.println(e);
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
